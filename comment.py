2b9787e538aa621eb50c8e22775977ef393a8cca
Yashu_Krish11 <89150253+yashu1wwww@users.noreply.github.com>
2022-07-23 22:49:55 +0530

    Update comment.py

--- comment.py
+++ comment.py
@@ -17,11 +17,11 @@ time.sleep(5)
 driver.get("https://twitter.com/imVkohli/status/1548216619641368577") #change to url to which post you want to put auto comment
 time.sleep(15)
 edit = driver.find_element_by_class_name('public-DraftStyleDefault-ltr')
-edit.send_keys(f"one century makes good") #change to your comment text
+edit.send_keys("one century makes good") #change to your comment text
 driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/section/div/div/div[1]/div/div/div[2]/div/div[2]/div[2]/div/div/div/div[2]/div[3]/div/div/div[2]/div[2]/div/span/span').click()
 time.sleep(3)
 edit = driver.find_element_by_class_name('public-DraftStyleDefault-ltr')
-edit.send_keys(f"waiting for vintage form") #change to your comment here
+edit.send_keys("waiting for vintage form") #change to your comment here
 driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/section/div/div/div[1]/div/div/div[2]/div/div[2]/div[2]/div/div/div/div[2]/div[3]/div/div/div[2]/div[2]/div/span/span').click()
 time.sleep(30)
 
