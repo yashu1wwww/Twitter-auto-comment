70e68e645b4050aad0d03cafebe33503169e1ca9
Yashu_Krish11 <89150253+yashu1wwww@users.noreply.github.com>
2022-07-31 10:21:28 +0530

    Update comment.py

--- comment.py
+++ comment.py
@@ -23,7 +23,8 @@ time.sleep(3)
 input = driver.find_element_by_class_name('public-DraftStyleDefault-ltr')
 input.send_keys("waiting for vintage form") #change to your comment here
 driver.find_element_by_css_selector('#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div > div > section > div > div > div:nth-child(2) > div > div > div.css-1dbjc4n.r-14lw9ot.r-184en5c > div > div.css-1dbjc4n.r-14lw9ot.r-1f1sjgu > div:nth-child(2) > div > div > div > div.css-1dbjc4n.r-1iusvr4.r-16y2uox.r-1777fci.r-1h8ys4a.r-1bylmt5.r-13tjlyg.r-7qyjyx.r-1ftll1t > div:nth-child(3) > div > div > div:nth-child(2) > div.css-18t94o4.css-1dbjc4n.r-l5o3uw.r-42olwf.r-sdzlij.r-1phboty.r-rs99b7.r-19u6a5r.r-2yi16.r-1qi8awa.r-1ny4l3l.r-ymttw5.r-o7ynqc.r-6416eg.r-lrvibr > div > span > span').click()
-time.sleep(10)
+time.sleep(5)
+driver.close()
 
 #if you want more comments means add 23,24,25 line and change the comment text
 
