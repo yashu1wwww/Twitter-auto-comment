6b93b096d9b3369b7fcf1f41e2fd9ce9bbe5253e
Yashu_Krish11 <89150253+yashu1wwww@users.noreply.github.com>
2022-10-07 10:36:05 +0530

    Update comment.py

--- comment.py
+++ comment.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
-driver= webdriver.Chrome("chromedriver.exe")
+driver= webdriver.Chrome()
 driver.maximize_window()
 
 driver.get("https://twitter.com/i/flow/login")
